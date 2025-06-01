584aa53423afe3009cb609ffc07e763add9f95af
YH Deng <127965357+Phy-D@users.noreply.github.com>
2025-06-01 14:26:11 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 REM 启动前端Next.js（生产模式）
-start "Next.js Frontend" cmd /k "cd front && npm run dev"
+start "Next.js Frontend" cmd /k "cd front && npm run build && npm run start -- -H 0.0.0.0 -p 3000"
 
 REM 启动后端Flask（指定Python环境）
 start "Flask Backend" cmd /k "cd server && "D:\myenvs\translate\python.exe" app.py"
